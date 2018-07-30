9fd677fdd73166127ebe82c8e707911dd89e24a4
Courtney Adams <cadams8238@gmail.com>
2018-07-30 09:35:48 -0700

    added lor characters

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ class HashMap {
             }
         }
     }
-    
+
 
     remove(key) {
         const index = this._findSlot(key);
@@ -81,3 +81,26 @@ class HashMap {
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
+
+
+function main() {
+  let lor = new HashMap();
+
+  lor.set("Hobbit", "Bilbo");
+  lor.set("Hobbit", "Frodo");
+  lor.set("Wizard", "Gandolf");
+
+  // console.log(lor.get("Hobbit"));
+  lor.set("Human", "Aragon");
+  lor.set("Elf", "Legolas");
+  lor.set("Maiar", "The Necromancer");
+  lor.set("Maiar", "Sauron");
+  lor.set("RingBearer", "Gollum");
+  lor.set("LadyOfLight", "Galadriel");
+  lor.set("HalfElven", "Arwen");
+  lor.set("Ent", "Treebeard");
+
+  console.log(lor);
+}
+
+console.log(main());
